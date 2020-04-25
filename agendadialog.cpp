9ff022bfae501c3f54e09b88246175c6f4711557
Mateusz Migot <migotmateusz@gmail.com>
2020-04-25 13:21:01 +0200

    Agenda Dialog added

--- agendadialog.cpp
+++ agendadialog.cpp
@@ -1,6 +1,10 @@
 #include <QHBoxLayout>
 #include <QLabel>
-#include <QRect>
+#include <QRectF>
+#include <QAction>
+#include <QPalette>
+#include <QTableView>
+#include <QHBoxLayout>
 #include "agendadialog.h"
 #include "ui_agendadialog.h"
 
@@ -10,9 +14,38 @@ AgendaDialog::AgendaDialog(DataAggregator *dman, QWidget *parent) :
 {
     ui->setupUi(this);
     this->datamanager = dman;
-    this->geometry();
+    int w=600, h=0;
+    QPixmap pixmap(10,10);
+    QVBoxLayout *mainlayout = new QVBoxLayout(this);
+    for(auto i : this->datamanager->get_events()){
+        if(QDate::currentDate().month() == i.get_date().date().month() &&
+                QDate::currentDate().day() <= i.get_date().date().day()){
+            //pixmap.fill(i.getcalendar()->getColor());
+            QPalette pal;
 
-    ui->listWidget->resize(1000,1000);
+            pal.setColor(QPalette::Background,i.getcalendar()->getColor());
+            this->resize(w,h);
+            QHBoxLayout *layout = new QHBoxLayout();
+            mainlayout->addLayout(layout);
+            QLabel *label = new QLabel();
+            QLabel *event = new QLabel();
+            event->setText(i.get_name().c_str());
+            event->setPalette(pal);
+            event->setFont(QFont(this->font().family(), 14));
+            event->setAutoFillBackground(true);
+            event->setContentsMargins(10,0,0,0);
+            label->setText(i.get_date().toString());
+            label->setFont(QFont(this->font().family(), 14));
+           /* label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+            label->setAlignment(Qt::AlignBottom | Qt::AlignLeft);
+            label->setGeometry(QRect(0,h/2,600,40));
+            */
+            layout->addWidget(label);
+            layout->addWidget(event);
+            h += 50;
+
+        }
+    }
 }
 
 AgendaDialog::~AgendaDialog()
